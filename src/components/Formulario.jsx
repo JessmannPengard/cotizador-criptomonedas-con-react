559d8e37c5047a5c4d8a742142da2a0d511f3163
JessmannPengard <contacto@jessmann.com>
2023-10-03 10:59:22 +0200

    selector criptos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
@@ -24,11 +24,36 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [SelectFiat] = useSelectMonedas('Elige tu moneda FIAT', monedas);
+    const [criptos, setCriptos] = useState([]);
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda FIAT', monedas);
+    const [criptomoneda, SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda', criptos);
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const listadoCriptos = resultado.Data.map(cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto;
+            });
+
+            setCriptos(listadoCriptos);
+
+        }
+        consultarAPI();
+    }, []);
 
     return (
         <form >
-            <SelectFiat />
+            <SelectMonedas />
+
+            <SelectCriptomonedas />
 
             <InputSubmit
                 type="submit"
